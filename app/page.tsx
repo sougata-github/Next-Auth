e2cb77a19d4f6062c9d4b9aeeaf8a2d8dc777f6c
Sougata <sougatadas9874@gmail.com>
2024-03-12 22:00:57 +0530

    setup project, add shadcn ui.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <h1 className="text-xl">Hello</h1>;
-}
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="text-4xl flex gap-x-4 p-20">
+      Next Auth
+      <Button size="lg" className="bg-indigo-500 text-white ">
+        Login
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
