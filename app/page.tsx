e0632e812aaab30eb9f9ed5e79168122f81d0d49
Sougata <sougatadas9874@gmail.com>
2024-03-14 19:42:01 +0530

    add hompage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
+import LoginButton from "@/components/auth/LoginButton";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 const Page = () => {
   return (
-    <div className="text-4xl flex gap-x-4 p-20">
-      Next Auth
-      <Button size="lg" className="bg-indigo-500 text-white ">
-        Login
-      </Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔒Auth
+        </h1>
+        <p className="text-lg text-white">A simple authentication service.</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 };
 
